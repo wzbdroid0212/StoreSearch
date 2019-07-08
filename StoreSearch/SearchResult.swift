822b2902d2b73610fedd00269a6a0ce57c6c9d7d
wuzhengbin <wzbdroid@gmail.com>
2019-07-08 10:01:32 +0800

    Add a search bar

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by wuzhengbin on 2019/7/8.
+//  Copyright © 2019 wuzhengbin. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
