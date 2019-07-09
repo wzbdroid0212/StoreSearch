b8c71898b40c304a3512e748ba71985da3a29735
wuzhengbin <wzbdroid@gmail.com>
2019-07-09 13:40:09 +0800

    将 segment.selectedindex 变为枚举类型

--- StoreSearch/Controllers/SearchViewController.swift
+++ StoreSearch/Controllers/SearchViewController.swift
@@ -119,14 +119,16 @@ extension SearchViewController: UISearchBarDelegate {
     }
     
     func performSearch() {
-        search.performSearch(for: searchBar.text!, category: segmentedControl.selectedSegmentIndex, completion: { success in
-            if !success {
-                self.showNetworkError()
-            }
-            self.tableView.reloadData()
-        })
-        tableView.reloadData()
-        searchBar.resignFirstResponder()
+        if let category = Search.Category(rawValue: segmentedControl.selectedSegmentIndex) {
+            search.performSearch(for: searchBar.text!, category: category, completion: { success in
+                if !success {
+                    self.showNetworkError()
+                }
+                self.tableView.reloadData()
+            })
+            tableView.reloadData()
+            searchBar.resignFirstResponder()
+        }
     }
     
     
